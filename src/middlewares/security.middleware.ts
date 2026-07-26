feb3c95bbfa499804c3575a36e14c9d3b8f702c3
Godsfavour Jesse <godsfavourjesse@gmail.com>
2026-07-26 22:38:14 +0100

    Fix Suspense and client component build errors

--- src/middlewares/security.middleware.ts
+++ src/middlewares/security.middleware.ts
@@ -1,19 +1,32 @@
-import express from "express";
-import cors from "cors";
+import { Express } from "express";
 import helmet from "helmet";
-import compression from "compression";
+import cors from "cors";
+import { env } from "../config";
+
 
 export default function securityMiddleware(
-    app: express.Application
+    app: Express
 ) {
     app.use(helmet());
 
     app.use(
         cors({
-            origin: process.env.CLIENT_URL,
+            origin(origin, callback) {
+                // Allow Postman, mobile apps, curl, etc.
+                if (!origin) {
+                    return callback(null, true);
+                }
+
+                if (env.client.urls.includes(origin)) {
+                    return callback(null, true);
+                }
+
+                return callback(
+                    new Error("Origin not allowed by CORS")
+                );
+            },
+
             credentials: true,
         })
     );
-
-    app.use(compression());
 }
